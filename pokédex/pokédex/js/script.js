cbc5c426411d3ab042065af04fa54be573eff679
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-05-16 20:24:15 -0300

    Corrigindo arquivos

--- pokédex/pokédex/js/script.js
+++
@@ -1,8 +0,0 @@
-const alterarTema = document.querySelector("#botao-alterarTema");
-const body = document.querySelector("body");
-
-function temaEscuro() {
-  body.classList.toggle("modo-escuro");
-}
-
-alterarTema.addEventListener("click", temaEscuro);
