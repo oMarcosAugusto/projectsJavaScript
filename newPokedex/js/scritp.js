e8f9aaaa28db7916dddd0fe700971d1f5fb6a0c9
oMarcosAugusto <marcoosaugusto@hotmail.com>
2023-08-24 17:20:19 +0100

    Adicionando novo projeto de pokedex com novos conceitos

---
+++ newPokedex/js/scritp.js
@@ -0,0 +1,28 @@
+
+
+console.log(convertPokemonToLi);
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+};
+
+const pokemonList = document.querySelector("#pokemonsList");  
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML = newHtml;
+})
